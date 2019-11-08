68b457a3d24ad92fa0b7a1bf57a83ea323f1da0c
Kyle Cheung <kcheu4@uwo.ca>
2019-11-08 18:17:51 -0500

    connected server to front end

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,60 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            name: '',
+            greeting: ''
+        };
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
 
+    handleChange(e) {
+        this.setState({ name: e.target.value });
+    }
+
+    handleSubmit(e) {
+        e.preventDefault();
+        fetch(`/api/greeting?name=${encodeURIComponent(this.state.name)}`)
+            .then(res => res.json())
+            .then(state => this.setState(state));
+    }
+
+    render() {
+
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <p>
+                        Edit <code>src/App.js</code> and save to reload.
+                    </p>
+                    <form onSubmit={this.handleSubmit}>
+                        <label htmlFor="name">Enter your name: </label>
+                        <input
+                            id="name"
+                            type="text"
+                            value={this.state.name}
+                            onChange={this.handleChange}
+                        />
+                        <button type="submit">Submit</button>
+                    </form>
+                    <p>{this.state.greeting}</p>
+                    <a
+                        className="App-link"
+                        href="https://reactjs.org"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        Learn React
+                    </a>
+                </header>
+            </div>
+        );
+    }
+}
 export default App;
--- server/server.js
+++ server/server.js
@@ -1,1 +1,15 @@
-console.log("hello");
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: false}));
+
+app.get('/api/greeting', (req, res) => {
+    const name = req.query.name || 'World';
+    res.setHeader('Content-Type', 'application/json');
+    res.send(JSON.stringify({ greeting: `Hello ${name}! from the server` }));
+});
+
+app.listen(5000, () =>
+    console.log('Express server is running on 5000')
+);
