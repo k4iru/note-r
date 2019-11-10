f5bdf11ce4512eb7536ea03d478a1f0bd060b4d8
Kyle Cheung <kcheu4@uwo.ca>
2019-11-10 17:23:28 -0500

    connected database

--- server/server.js
+++ server/server.js
@@ -1,15 +1,30 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const port = 5000;
+const Pool = require('pg').Pool;
+
+const pool = new Pool({
+    user: 'kyle',
+    host: 'localhost',
+    database: 'api',
+    password: '',
+    port: 5432,
+})
+
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false}));
 
 app.get('/api/greeting', (req, res) => {
-    const name = req.query.name || 'World';
-    res.setHeader('Content-Type', 'application/json');
-    res.send(JSON.stringify({ greeting: `Hello ${name}! from the server` }));
+    pool
+        .query('SELECT NOW() as now')
+        .then(res => console.log(res.row[0]))
+        .catch(e => console.error(e.stack))
+    //const name = req.query.name || 'World';
+    //res.setHeader('Content-Type', 'application/json');
+    //res.send(JSON.stringify({ greeting: `Hello ${name}! from the server` }));
 });
 
-app.listen(5000, () =>
-    console.log('Express server is running on 5000')
+app.listen(port, () =>
+    console.log(`Express server is running on ${port}`)
 );
