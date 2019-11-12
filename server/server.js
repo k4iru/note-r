58232eb344dc1d72d20e55dad210c1e1cf47ecb5
Kyle Cheung <kcheu4@uwo.ca>
2019-11-11 21:19:03 -0500

    fixed db to client connection

--- server/server.js
+++ server/server.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const helmet = require('helmet');
+const cors = require('cors');
 const bodyParser = require('body-parser');
 const port = 5000;
 const { Pool } = require('pg');
 
-const pool = new Pool({
+const client = new Pool({
     user: 'kyle',
     host: 'localhost',
     database: 'api',
@@ -13,16 +15,29 @@ const pool = new Pool({
 
 const app = express();
 
+const whitelist = ['http://localhost:3000']
+const corsOptions = {
+    origin: function (origin, callback) {
+        if (whitelist.indexOf(origin) !== -1 || !origin) {
+            callback(null, true)
+        } else {
+            callback(new Error('Not allowed by CORS'))
+        }
+    }
+}
+
+app.use(helmet());
+app.use(cors(corsOptions))
 app.use(bodyParser.urlencoded({ extended: false}));
 
 app.get('/api/greeting', (req, res) => {
-    pool
-      .query('SELECT markdown FROM mark')
-      .then(res => console.table(res.rows[0]))
-      .catch(e => console.error(e.stack))
-    const name = req.query.name || 'World';
-    res.setHeader('Content-Type', 'application/json');
-    res.send(JSON.stringify({ text: `hello ${res.rows}` }));
+    client
+        .query('SELECT markdown FROM mark')
+        .then(result => {
+            console.table(result.rows[0].markdown)
+            res.send(JSON.stringify({ text: result.rows[0].markdown }));
+        })
+        .catch(e => console.error(e.stack))
 });
 
 app.listen(port, () =>
